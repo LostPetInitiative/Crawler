515d8761ba0e85783babae9fd9ae7c667d643f06
Dmitry Grechka <dmitry@grechka.family>
2021-10-08 19:53:20 +0300

    project cleanup & drone pipeline

---
+++ Crawler/SemanticTypes.fs
@@ -0,0 +1,36 @@
+﻿namespace HtmlCrawler
+
+module SemanticTypes =
+    type Animal = 
+        |   dog = 1
+        |   cat = 2
+    type Sex = 
+    |   unknown = 1
+    |   male = 2
+    |   female = 3
+    type CardType =
+    |   lost = 1
+    |   found = 2
+    type PetPhoto = {
+        /// Filename with extension
+        id: string
+        ThumbHostRelativeURL: string
+    }
+    type Author = {
+        username: string
+        phone: string
+        email: string
+    }
+    type PetCard = {
+        art: string
+        photos : PetPhoto[]
+        animal: Animal
+        sex: Sex
+        address: string
+        latitude: string
+        longitude: string
+        date: string // %Y-%m-%dT%H:%M:%SZ
+        ``type``: CardType
+        description: string
+        author: Author
+    }
